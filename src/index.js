287f07078514fd764bd8705471b12cfe9151002c
Tiago rosa da costa <tiagorosadacost@gmail.com>
2022-01-06 23:13:15 -0300

    Send code to remote repository

---
+++ src/index.js
@@ -0,0 +1,38 @@
+require("dotenv").config()
+const express = require("express")
+const multer = require("multer")
+const multerS3 = require("multer-s3")
+const aws = require("aws-sdk");
+const app = express()
+const s3 = new aws.S3({ region: "us-east-1" })
+
+const upload = multer({
+    storage: multerS3({
+      s3: s3,
+      bucket: process.env.S3_BUCKET,
+      metadata: function (req, file, cb) {
+        cb(null, {fieldName: file.fieldname});
+      },
+      key: function (req, file, cb) {
+        cb(null, `${file.fieldname}${Date.now().toString()}`)
+      }
+    })
+  })
+
+app.post("/files", upload.single("file"), (request, response) => {
+    response.status(201).json({
+        linkAccessFile: `http://localhost:3000/link-files/${request.file.key}`,
+        key: request.file.key
+    })
+})
+
+app.get("/link-files/:fileId", (request, response) => {
+    const presignedUrl = s3.getSignedUrl('getObject', {
+        Bucket: process.env.S3_BUCKET,
+        Key: request.params.fileId, 
+        Expires: 100 
+    });
+
+    return response.redirect(presignedUrl)
+})
+app.listen(3000, () => console.log("Server is running at http://localhost:3000"))
